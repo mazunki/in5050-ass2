79a3f92b0f60a1bb227a9ea02b8786aa8e3fadf4
Mazunki Hoksaas <rolferen@gmail.com>
2025-03-27 15:49:46 +0100

    add pthread-compatible wrappers for dct_idct_{Y,U,V}
    also minor reordering of functions for readability

--- project/src/quantdct.cu
+++ project/src/quantdct.cu
@@ -2,16 +2,11 @@
 #include <nvToolsExt.h>
 #include "quantdct.h"
 
-void dequantize_idct_Y(struct c63_common *cm) {
-  dequantize_idct(cm->curframe->residuals->Ydct, cm->curframe->predicted->Y, cm->ypw, cm->yph, cm->curframe->recons->Y, cm->quanttbl[Y_COMPONENT]);
-}
-void dequantize_idct_U(struct c63_common *cm) {
-  dequantize_idct(cm->curframe->residuals->Udct, cm->curframe->predicted->U, cm->upw, cm->uph, cm->curframe->recons->U, cm->quanttbl[U_COMPONENT]);
-}
-void dequantize_idct_V(struct c63_common *cm) {
-  dequantize_idct(cm->curframe->residuals->Vdct, cm->curframe->predicted->V, cm->vpw, cm->vph, cm->curframe->recons->V, cm->quanttbl[V_COMPONENT]);
-}
-
+/** quantize (slow CPU-only function)
+ *   @param[in]  orig
+ *   @param[in]  predicted
+ *   @param[out] residuals
+ */
 void dct_quantize_Y(struct c63_common *cm) {
   dct_quantize(cm->curframe->orig->Y, cm->curframe->predicted->Y, cm->padw[Y_COMPONENT], cm->padh[Y_COMPONENT], cm->curframe->residuals->Ydct, cm->quanttbl[Y_COMPONENT]);
 }
@@ -20,6 +15,21 @@ void dct_quantize_U(struct c63_common *cm) {
 }
 void dct_quantize_V(struct c63_common *cm) {
   dct_quantize(cm->curframe->orig->V, cm->curframe->predicted->V, cm->padw[V_COMPONENT], cm->padh[V_COMPONENT], cm->curframe->residuals->Vdct, cm->quanttbl[V_COMPONENT]);
+}
+
+/** dequantize (slow CPU-only function)
+ *   @param[in]  residuals
+ *   @param[in]  predicted
+ *   @param[out] recons
+ */
+void dequantize_idct_Y(struct c63_common *cm) {
+  dequantize_idct(cm->curframe->residuals->Ydct, cm->curframe->predicted->Y, cm->ypw, cm->yph, cm->curframe->recons->Y, cm->quanttbl[Y_COMPONENT]);
+}
+void dequantize_idct_U(struct c63_common *cm) {
+  dequantize_idct(cm->curframe->residuals->Udct, cm->curframe->predicted->U, cm->upw, cm->uph, cm->curframe->recons->U, cm->quanttbl[U_COMPONENT]);
+}
+void dequantize_idct_V(struct c63_common *cm) {
+  dequantize_idct(cm->curframe->residuals->Vdct, cm->curframe->predicted->V, cm->vpw, cm->vph, cm->curframe->recons->V, cm->quanttbl[V_COMPONENT]);
 }
 
 void dct_idct_Y(struct c63_common *cm) {
@@ -48,7 +58,25 @@ void dct_idct_V(struct c63_common *cm) {
   dct_quantize_V(cm);
   nvtxRangePop(); // dct
 
-  nvtxRangePush("idct V");
+  nvtxRangePush("idct");
   dequantize_idct_V(cm);
   nvtxRangePop(); // idct
+}
+
+void *pthread_dct_idct_Y(void *ptr) {
+  dct_idct_Y((struct c63_common *) ptr);
+
+  return NULL;
+}
+
+void *pthread_dct_idct_U(void *ptr) {
+  dct_idct_U((struct c63_common *) ptr);
+
+  return NULL;
+}
+
+void *pthread_dct_idct_V(void *ptr) {
+  dct_idct_V((struct c63_common *) ptr);
+
+  return NULL;
 }
